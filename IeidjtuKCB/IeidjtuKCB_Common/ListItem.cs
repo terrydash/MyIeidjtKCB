8cbfc0720692e21e2e6e73f65c580b62da7d718a
terrydash <53047801@qq.com>
2016-02-05 20:25:49 +0800

    ComboBox数据源可以绑定

---
+++ IeidjtuKCB/IeidjtuKCB_Common/ListItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IeidjtuKCB_Common
+{
+    /// <summary>
+    /// 选择项类，用于ComboBox或者ListBox添加项
+    /// </summary>
+    public class ListItem
+    {
+        private string id = string.Empty;
+        private string name = string.Empty;
+        public ListItem(string sid, string sname)
+        {
+            id = sid;
+            name = sname;
+        }
+        public override string ToString()
+        {
+            return this.name;
+        }
+        public string ID
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    }
+}
