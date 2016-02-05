0a589c265f35e477254bb865139e7a91c580695d
terrydash <53047801@qq.com>
2016-02-05 19:32:10 +0800

    建立文档

---
+++ IeidjtuKCB/IeidjtuKCB_Common/DB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dos.ORM;
+namespace IeidjtuKCB_Common
+{
+    public class DB
+    {
+        public static readonly DbSession Context = new DbSession(DatabaseType.SqlServer9, "Data Source=127.0.0.1;Database=DBName;User Id=root;Password=root;");
+    }
+}
