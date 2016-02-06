97f41e2f54175a786526ce3997a943f706dc1fb7
terrydash <53047801@qq.com>
2016-02-06 15:27:58 +0800

    绑定COMBOBOX数据源写入COMMON类

---
+++ IeidjtuKCB/IeidjtuKCB_BLL/ActiveYear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IeidjtuKCB_Model;
+namespace IeidjtuKCB_BLL
+{/// <summary>
+/// 
+/// </summary>
+    public class Activeyear
+    {
+       
+    }
+}
--- IeidjtuKCB/IeidjtuKCB_DAL/ActiveYear.cs
+++ IeidjtuKCB/IeidjtuKCB_DAL/ActiveYear.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace IeidjtuKCB_BLL
+namespace IeidjtuKCB_DAL
 {
-    public class Class1
+    public class ActiveYear
     {
+        interface IGetActivYearName {
+
+
+
+        };
+
     }
 }
