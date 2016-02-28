03f7700c82a012ac2ca3d666398f3a9a618636f0
terry dash <mybest13@163.com>
2016-02-28 11:20:59 +0800

    KCB的HTML建立 准备把HTML的替换利用MODEL做好

---
+++ IeidjtuKCB/IeidjtuKCB_Common/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IeidjtuKCB_Common
+{/// <summary>
+/// 生成静态文件类
+/// </summary>
+    public static class FileHelper
+    {
+        public static void WrtieHTML(string FileName,List <string> ReplaceStrings,)
+        {
+
+
+        }
+    }
+}
---
+++ IeidjtuKCB/IeidjtuKCB_Model/HTMLReplaceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IeidjtuKCB_Model
+{
+    public class HTMLReplaceModel<T>
+    {
+        private string TextName;
+        private string ReplaceText;
+       
+    }
+    
+
+}
