cd0d8eb7ae6fdab8168825ad120fb705676e5013
terry dash <mybest13@163.com>
2016-02-29 16:10:58 +0800

    DB类加入执行SQL功能

--- IeidjtuKCB/Ieidjtu.DBSession/DB.cs
+++ IeidjtuKCB/Ieidjtu.DBSession/DB.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Dos.ORM;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+
 namespace IeidjtuKCB.DBSession
 {   /// <summary>
     /// 数据库连接上下文
     /// </summary>
-    public class DB
+    public  class DB
     {
-        public static readonly DbSession Context = new DbSession(DatabaseType.SqlServer9, @"Data Source=.\XUGUOXU54;Database=Education;User Id=sa;Password=52Xuguoxu;");
+        public static  DbSession Context = new DbSession(DatabaseType.SqlServer9, @"Data Source=.\XUGUOXU54;Database=Education;User Id=sa;Password=52Xuguoxu;");
+        /// <summary>
+        /// 执行带参数的SQL语句
+        /// </summary>
+        /// <param name="sql">要执行的SQL语句</param>
+        /// <param name="parameters">参数</param>
+        /// <returns>返回影响的数目</returns>
+        public int ExecutNoneQuery(string sql, IList<DbParameter> parameters)
+        {
+            DbCommand db = Context.Db.GetSqlStringCommand(sql);
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                foreach (DbParameter parameter in parameters)
+                {
+                    db.Parameters.Add(parameter);
+                }
+            }
+           
+            return Context.ExecuteNonQuery(db);
+        }
     }
 }
