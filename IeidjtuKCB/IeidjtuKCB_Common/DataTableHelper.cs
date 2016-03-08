94297822b11c1d839f560e8d9fb7e2160777cb57
terrydash <53047801@qq.com>
2016-03-08 22:55:05 +0800

    生成课程表EXCEL功能成功

---
+++ IeidjtuKCB/IeidjtuKCB_Common/DataTableHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+
+namespace IeidjtuKCB.Common
+{
+    public static class DataTableHelper
+    {   /// <summary>
+        /// 获取DATATABLE的列名
+        /// </summary>
+        /// <param name="dt">要获取的DATATABLE</param>
+        /// <returns>列名的泛型集合</returns>
+        static List<string >GetDataTableColumns(System.Data.DataTable dt)
+        {
+            if (dt != null)
+            {
+
+                if (dt.Columns.Count > 0)
+                {
+                    List<string> strList = new List<string>();
+
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        strList.Add(dt.Columns[i].ColumnName);
+                    }
+                    return strList;
+                }
+
+
+            }
+                return null;
+
+           
+
+        }
+
+    }
+}
